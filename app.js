9596dec7ab85100fa872aa136e032f287eb9fd38
Jiell Richardson <32615427+jdonric@users.noreply.github.com>
2019-06-23 13:53:53 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+//VARIABLES + FILES
+var express = require('express');
+var nodemailer = require('nodemailer'); //dev 
+var bodyParser = require('body-parser');
+const router = express.Router();
+var ejs = require('ejs');
+var ejsLint = require('ejs-lint');
+const mongoose = require('mongoose');
+
+
+var app = express();
+app.set('view engine', 'ejs');
+
+// use body-parser middleware
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+//db
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert'); //???
+
+// Connection URL
+const url = mongoose.connect("mongodb://localhost:27017/yby", { useNewUrlParser: true });
+mongoose.Promise = global.Promise; 
+
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, client) {
+  console.log('Finally CONNECTED')
+});
+
+
+//BASIC GET ROUTES
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/order', function(req, res){
+    res.render('order');
+});
+
+
+
+
+//LISTENING
+app.listen(4000, function (req, res){
+    console.log(`TUNED INTO 4000`);
+});
